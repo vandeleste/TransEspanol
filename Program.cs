d3134131406058e5ffb5d9ef4c8a21cc52f273c8
vandeleste <114308984+vandeleste@users.noreply.github.com>
2026-06-27 13:40:03 -0300

    partir una nota funciona, la parte en dos

--- Program.cs
+++ Program.cs
@@ -31,14 +31,14 @@ class Program{
         }
         //Cambia la longitud de la nota
         int length_note=int.Parse(Data_OpenUtau[j+1].Substring(7));//Length=480
-        Data_OpenUtau[j]="Length="+length_note/2;
-        //Busca el final de esta nota
+        Data_OpenUtau[j+1]="Length="+length_note/2;
+        //Ubicacion de la proxima data.Ej [#0xxx]
         int k=j+1;
         while(k<Data_OpenUtau.Length && !Data_OpenUtau[k].StartsWith("[#0")){
             k++;
         }
         //Guarda en un array aux la data
-        string[] Data_aux_1=new string[k];//Creo un array que va a tener los primeros k elementos
+        string[] Data_aux_1=new string[k];//Crea un array que va a tener los primeros k elementos.(hasta la nota selccionada)
         Array.Copy(Data_OpenUtau, 0, Data_aux_1, 0, k);
 
         string[] Data_aux_2=new string[Data_OpenUtau.Length-k];//Va a quedarse con el resto del array
@@ -46,12 +46,22 @@ class Program{
         //Crea la nueva nota copiando la anterior pero cambiando la longitud
         string[] Data_note=new string[k-j];
         Array.Copy(Data_OpenUtau,j,Data_note,0,k-j);
-        Data_note[k+1]="Length="+(length_note/2  +  length_note%2).ToString();
+        Data_note[0]="[#INSERT]";
+        Data_note[1]="Length="+(length_note/2  +  length_note%2).ToString();
         //Guarda los cambios
         Data_OpenUtau =new string[Data_aux_1.Length+Data_aux_2.Length+Data_note.Length];
-        Array.Copy(Data_OpenUtau,0,Data_aux_1,0,k);
-        Array.Copy(Data_OpenUtau,k+1,Data_note,0,k-j);
-        Array.Copy(Data_OpenUtau,k+k-j,Data_aux_2,0,k-j);
+        Array.Copy(Data_aux_1,0,Data_OpenUtau,0,Data_aux_1.Length);
+        Array.Copy(Data_note,0,Data_OpenUtau,k,Data_note.Length);
+        Array.Copy(Data_aux_2,0,Data_OpenUtau,k+Data_note.Length,Data_aux_2.Length);
+        //Guardo los cambios
+        File.WriteAllLines(path_temp,Data_OpenUtau);
+
+
+//muestro los datos del temp
+        for (int i=0;i<Data_OpenUtau.Length ;i++){
+            File.AppendAllText(path_log,Data_OpenUtau[i]+"\n");
+        }
+
 //
         for (int i=0; i < Data_OpenUtau.Length; i++){
             if(Data_OpenUtau[i].StartsWith("Lyric=")){
@@ -66,5 +76,8 @@ class Program{
         File.WriteAllLines(path_temp,Data_OpenUtau);
         File.AppendAllText(path_log,"Finaliza Plugin\n");
     }
+/*
+void PartirEnDos(int:x,int:y)// proporcion x:y cada "x" elementos hay "y" elementos
+*/    
     
 }
